fd8e4606802a3a130490c53e55a8eb90ee483178
tonghui <82929844@qq.com>
2019-12-08 22:58:20 +0800

    commit

---
+++ src/main/java/com/gupao/springbootdemo/controller/K8S1Controller.java
@@ -0,0 +1,14 @@
+package com.gupao.springbootdemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class K8S1Controller {
+
+
+    @RequestMapping("/k8s")
+    public String k8s(){
+        return "hello K8s <br/>111222 ";
+    }
+}
